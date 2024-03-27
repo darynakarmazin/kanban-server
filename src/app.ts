f5e816fcf39bcc95731b64baef1d064be116a1b1
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-27 10:29:04 +0200

    Created connection to DB

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
-import express, { Request, Response } from "express";
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import logger from "morgan";
+import cors from "cors";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
-});
+// const kanbansRouter = require("./routes/api/kanbans");
+
+const app = express();
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+if (!DB_HOST) {
+  console.error("DB_HOST is not defined in the environment variables.");
+  process.exit(1);
+}
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/kanbans", kanbansRouter);
 
 export default app;
